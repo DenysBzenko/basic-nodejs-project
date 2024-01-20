0001f16b9620bababe44b2b6d5ba9940cb4234d3
Denys Bzenko <dbzenko@kse.org.ua>
2024-01-20 22:24:19 +0200

    Add CORS

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); 
 const app = express();
 const port = 3001;
 
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors());
 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 const cat = require('./data/cat.json');
 const dog = require('./data/dog.json');
 const documentation = require('./data/doc.json'); 
-
 
 app.get('/html1', (req, res) => {
     res.sendFile(path.join(__dirname, 'assets/pages/page1.html'));
@@ -20,12 +21,10 @@ app.get('/html2', (req, res) => {
     res.sendFile(path.join(__dirname, 'assets/pages/page2.html'));
 });
 
-
 app.get('/file/:filename', (req, res) => {
     const filename = req.params.filename;
     res.sendFile(path.join(__dirname, 'assets', filename));
 });
-
 
 app.get('/objects/:type/:id', (req, res) => {
     const { type, id } = req.params;
@@ -36,17 +35,15 @@ app.get('/objects/:type/:id', (req, res) => {
 app.get('/objects/:type', (req, res) => {
     const type = req.params.type;
     res.json(type === 'cat' ? cat : dog);
-}); 
+});
 
 app.get('/objects', (req, res) => {
     res.json({ cat, dog });
 });
 
-
 app.get('/info', (req, res) => {
     res.json(documentation);
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
